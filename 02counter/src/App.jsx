bf0401a6501cbfd8713879502d05122421796405
akheruddinahmed <akheruddinahmed@gmail.com>
2026-06-04 14:52:02 +0530

    Interview

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -3,28 +3,31 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [counter, setCounter] = useState(0);
-  //et counter = 5;
+  const [counter, setCounter] = useState(15);
+
+  //let counter = 15
 
   const addValue = () => {
-    counter = counter + 1;
-    setCounter(counter);
-    //console.log("clicked ", counter);
+    //counter = counter + 1
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
   };
 
-  const remValue = () => {
+  const removeValue = () => {
     setCounter(counter - 1);
-    //console.log(counter);
   };
 
   return (
     <>
-      <h1>Chai Aur React</h1>
-      <h2>Counter Value : {counter}</h2>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
 
-      <button onClick={addValue}>Add Value</button>
-      <br></br>
-      <button onClick={remValue}>Remove Value</button>
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={removeValue}>remove value {counter}</button>
+      <p>footer: {counter}</p>
     </>
   );
 }
